c4639de196734f10fd0883f0e2ab349de235e0e7
LAPTOP-81IJOBPV\user <1125899906842624abc@gmail.com>
2024-06-19 10:42:14 +0800

    commit 6/19 #timliin

--- project/frontend/src/main.js
+++ project/frontend/src/main.js
@@ -7,7 +7,7 @@ import VueApexCharts from 'vue-apexcharts'
 Vue.use(VueApexCharts)
 
 Vue.component('apexchart', VueApexCharts)
-Vue.prototype.$apiUrl = 'https://8dec-2401-e180-8940-6dd-a5e2-a8a7-477e-5295.ngrok-free.app';
+Vue.prototype.$apiUrl = 'https://1084-2401-e180-8940-6dd-a5e2-a8a7-477e-5295.ngrok-free.app';
 Vue.prototype.$axios = axios;
 Vue.prototype.$userId = null;
 Vue.prototype.$userName = null;// 6/2
